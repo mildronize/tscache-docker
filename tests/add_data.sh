becf5e06ed2683840cb9ddc6efed465387f5b0a7
Thada Wangthammang <mildronize@gmail.com>
2017-03-09 23:24:51 +0700

    fix

--- tests/add_data.sh
+++ tests/add_data.sh
@@ -4,12 +4,11 @@ set -x
 
 tsdb_cli_path="./tsdb"
 service_name="tsdb-test"
-import_file="dps-1"
+import_file="./tests/data/dps-1"
 docker_compose_path="docker-compose-test.yml"
 
 container_id=`docker-compose --file $docker_compose_path ps -q $service_name`
 
 pwd
 
-docker cp ./data/$import_file $container_id:/opt/opentsdb/opentsdb-2.3.0/data \ 
-&& docker-compose --file $docker_compose_path exec $service_name $tsdb_cli_path import ./data/$import_file
+docker cp $import_file $container_id:/opt/opentsdb/opentsdb-2.3.0/data && docker-compose --file $docker_compose_path exec $service_name $tsdb_cli_path import ./data/$import_file
