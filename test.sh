fc6cdb76e394003050b3c91dc2fffcf4cb79869a
Thada Wangthammang <mildronize@gmail.com>
2017-03-09 23:10:09 +0700

    fix path

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 echo "Running test suite..." \
     && pip install -r tests/requirements.txt \
-    && chmod a+x tests/test_query.py test/add_data.sh \
+    && chmod a+x tests/test_query.py tests/add_data.sh \
     && tests/add_data.sh \
     && tests/test_query.py \
