5eb292eddbb5d56461e48de083e701cf4ff7233c
Thada Wangthammang <mildronize@gmail.com>
2017-03-09 23:16:11 +0700

    fix path for build

--- tests/add_data.sh
+++ tests/add_data.sh
@@ -3,8 +3,11 @@
 tsdb_cli_path="./tsdb"
 service_name="tsdb-test"
 import_file="dps-1"
+docker_compose_path="docker-compose-test.yml"
 
-container_id=`docker-compose ps -q $service_name`
+container_id=`docker-compose --file $docker_compose_path ps -q $service_name`
+
+pwd
 
 docker cp ./data/$import_file $container_id:/opt/opentsdb/opentsdb-2.3.0/data \ 
-&& docker-compose exec $service_name $tsdb_cli_path import ./data/$import_file
+&& docker-compose --file $docker_compose_path exec $service_name $tsdb_cli_path import ./data/$import_file
