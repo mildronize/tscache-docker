01a1a4ed269c3abf86cb14726185f9bb65216a97
Thada Wangthammang <mildronize@gmail.com>
2017-03-09 23:39:21 +0700

    fix

--- tests/add_data.sh
+++ tests/add_data.sh
@@ -11,4 +11,5 @@ container_id=`docker-compose --file $docker_compose_path ps -q $service_name`
 
 pwd
 
-docker cp ./tests/data/$import_file $container_id:/opt/opentsdb/opentsdb-2.3.0/data && docker exec -i $container_id $tsdb_cli_path import ./data/$import_file
+docker cp ./tests/data/$import_file $container_id:/opt/opentsdb/opentsdb-2.3.0/data/$import_file && docker exec -i $container_id $tsdb_cli_path import ./data/$import_file
+
