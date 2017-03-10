6878451ed3445fff9e935329555ad3ca0aa05204
Thada Wangthammang <mildronize@gmail.com>
2017-03-10 11:38:36 +0700

    enable log  when opentsdb start

--- opentsdb/start_opentsdb.sh
+++ opentsdb/start_opentsdb.sh
@@ -2,15 +2,21 @@
 export TSDB_VERSION="{{TSDB_VERSION}}"
 # echo "Sleeping for 30 seconds to give HBase time to warm up"
 # sleep 30 
+log_path="/var/log/opentsdb"
+tsd_log="tsd.log"
+created_table_log="created_table.log"
 
 
 if [ ! -e /opt/opentsdb_tables_created.txt ]; then
 	echo "creating tsdb tables"
-	bash /opt/bin/create_tsdb_tables.sh
+	bash /opt/bin/create_tsdb_tables.sh > $log_path/$created_table_log 2>&1
 	echo "created tsdb tables"
 fi
 
 echo "starting opentsdb"
-/opt/opentsdb/opentsdb-${TSDB_VERSION}/build/tsdb tsd --port=4242 --staticroot=/opt/opentsdb/opentsdb-${TSDB_VERSION}/build/staticroot --cachedir=/tmp --auto-metric
+if [ -e $tsd_log ] ; then
+    rm -f $tsd_log
+fi
+/opt/opentsdb/opentsdb-${TSDB_VERSION}/build/tsdb tsd --port=4242 --staticroot=/opt/opentsdb/opentsdb-${TSDB_VERSION}/build/staticroot --cachedir=/tmp --auto-metric > $log_path/$tsd_log 2>&1
 
 # sleep infinity
